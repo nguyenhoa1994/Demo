301ed27c012a7c2a95f534ab03f17affcc9baa92
hoanv <nguyenvanhoasn1994@gmail.com>
2022-08-18 17:46:47 +0700

    Create class EditArticles

---
+++ src/entity/EditArticles.java
@@ -0,0 +1,10 @@
+package entity;
+
+import java.math.BigDecimal;
+
+public class EditArticles {
+    private BigDecimal oldStopLoss; /* Số lượng trước đó */
+    private BigDecimal curStopLoss; /* Số lượng hiện tại */
+    private BigDecimal oldTakeProfit;
+    private BigDecimal curTakeProfit;
+}
