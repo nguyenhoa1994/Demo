3138c4c53875c25313d4e1c6e1e4ae4e30c2ec68
hoanv <nguyenvanhoasn1994@gmail.com>
2022-08-18 22:17:45 +0700

    tesst

--- src/entity/Articles.java
+++ src/entity/Articles.java
@@ -1,4 +1,13 @@
 package entity;
 
+import java.math.BigDecimal;
+
 public class Articles {
+    private BigDecimal oldStopLoss; /* Số lượng trước đó */
+
+    private BigDecimal curStopLoss; /* Số lượng hiện tại */
+
+    private BigDecimal oldTakeProfit;
+
+    private BigDecimal curTakeProfit;
 }
--- src/entity/EditArticles.java
+++ src/entity/EditArticles.java
@@ -4,7 +4,10 @@ import java.math.BigDecimal;
 
 public class EditArticles {
     private BigDecimal oldStopLoss; /* Số lượng trước đó */
+
     private BigDecimal curStopLoss; /* Số lượng hiện tại */
+
     private BigDecimal oldTakeProfit;
+
     private BigDecimal curTakeProfit;
 }
